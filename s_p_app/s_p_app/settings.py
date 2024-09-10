7b5ca0be0031a2e96ad9e668c923f5177d582f24
Vova Molchanov <r1ken@mail.ru>
2024-09-10 21:56:43 +0300

    end tutorial07

--- s_p_app/s_p_app/settings.py
+++ s_p_app/s_p_app/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 's_p_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
